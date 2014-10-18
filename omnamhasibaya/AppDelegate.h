c0cfa82fcfaedc9f600b983432a36e308684d7d4
JITEN <jiten@JITENs-Mac-mini.local>
2014-10-18 04:51:20 -0700

    Initial Commit

---
+++ omnamhasibaya/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  omnamhasibaya
+//
+//  Created by JITEN on 10/18/14.
+//  Copyright (c) 2014 JITEN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ omnamhasibaya/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  omnamhasibaya
+//
+//  Created by JITEN on 10/18/14.
+//  Copyright (c) 2014 JITEN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
