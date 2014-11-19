7d055ce529a1bc3e2f6a34d7f51d37affe9d1544
Bsetecip10 <bsetecip10@Nirmals-Mac-mini.local>
2014-11-19 18:49:32 +0530

    sm

--- omnamhasibaya/ViewController.h
+++ omnamhasibaya/ViewController.h
@@ -9,7 +9,10 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIButton *button;
+@property (weak, nonatomic) IBOutlet UITextField *textfield;
 
+- (IBAction)buttonclicked:(id)sender;
 
 @end
 
--- omnamhasibaya/ViewController.m
+++ omnamhasibaya/ViewController.m
@@ -8,20 +8,74 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDataSource,UITableViewDelegate>
 
 @end
 
 @implementation ViewController
+{
+    UITableView *table;
+}
+@synthesize button,textfield;
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
+    CGFloat height=44;
+    
+    table=[[UITableView alloc]initWithFrame:CGRectMake(0, 120, 320, height*[textfield.text intValue])];
+    table.delegate=self;
+    table.dataSource=self;
+    [self.view addSubview:table];
+    
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)buttonclicked:(id)sender
+{
+    if (textfield.text.length==0) {
+        UIAlertView *alert=[[UIAlertView alloc]initWithTitle:@"Warning !" message:@"enter no of row" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+        [alert show];
+    }
+    else
+    {
+        CGFloat height=44;
+
+        [table setFrame:CGRectMake(0, 120, 320, height*[textfield.text intValue])];
+        [table reloadData];
+    }
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    int i=[textfield.text intValue];
+    return i;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+
+        cell.textLabel.text=[NSString stringWithFormat:@"row %ld",indexPath.row+1];
+  
+       return cell;
+}
+- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+}
+
+
 
 @end
